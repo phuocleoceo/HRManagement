f48365afe90b0d90901bd9b0a31366797c48ebc3
phuocleoceo <ht10082001@gmail.com>
2021-06-21 11:22:01 +0700

    DPM Component Init

--- client-app/src/api/apiService.js
+++ client-app/src/api/apiService.js
@@ -4,40 +4,42 @@ let API_URL = "https://localhost:5001/api";
 let department = "department";
 let employee = "employee";
 
-export function callAPI(endpoint, method, body) {
-    return axios({
-        method,
-        url: `${API_URL}/${endpoint}`,
-        data: body
-    }).catch(e => {
-        console.log(e)
-    });
+export async function callAPI(endpoint, method, body) {
+    try {
+        return axios({
+            method,
+            url: `${API_URL}/${endpoint}`,
+            data: body
+        });
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 // DEPARTMENT
 export function GET_ALL_DEPARTMENT() {
-    return callApi(department, "GET");
+    return callAPI(department, "GET");
 };
 export function DELETE_DEPARTMENT(id) {
-    return callApi(department + "/" + id, "DELETE");
+    return callAPI(department + "/" + id, "DELETE");
 }
 export function POST_DEPARTMENT(body) {
-    return callApi(department, "POST", body);
+    return callAPI(department, "POST", body);
 }
 export function PUT_DEPARTMENT(id, body) {
-    return callApi(department + "/" + id, "PUT", body);
+    return callAPI(department + "/" + id, "PUT", body);
 }
 
 // EMPLOYEE
 export function GET_ALL_EMPLOYEE() {
-    return callApi(employee, "GET");
+    return callAPI(employee, "GET");
 };
 export function DELETE_EMPLOYEE(id) {
-    return callApi(employee + "/" + id, "DELETE");
+    return callAPI(employee + "/" + id, "DELETE");
 }
 export function POST_EMPLOYEE(body) {
-    return callApi(employee, "POST", body);
+    return callAPI(employee, "POST", body);
 }
 export function PUT_EMPLOYEE(id, body) {
-    return callApi(employee + "/" + id, "PUT", body);
+    return callAPI(employee + "/" + id, "PUT", body);
 }
--- client-app/src/components/Department.js
+++ client-app/src/components/Department.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { GET_ALL_DEPARTMENT } from '../api/apiService';
+import { Table } from 'react-bootstrap';
 
-export default function Department() {
+function Department() {
+	const [deps, setDeps] = useState([]);
+
+	useEffect(() => {
+		async function refreshList() {
+			const list = await GET_ALL_DEPARTMENT();
+			setDeps(list.data);
+		};
+		refreshList();
+	}, []);
+
+
 	return (
-		<div className="mt-5 d-flex justify-content-left">
-			This is Department Page 
+		<div >
+			<Table className="mt-4" striped bordered hover size="sm">
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Name</th>
+						<th>Options</th>
+					</tr>
+				</thead>
+				<tbody>
+					{deps.map(dep =>
+						<tr key={dep.Id}>
+							<td>{dep.Id}</td>
+							<td>{dep.Name}</td>
+							<td>Edit/Delete</td>
+						</tr>)}
+				</tbody>
+			</Table>
 		</div>
 	)
 }
+
+export default Department;
+
