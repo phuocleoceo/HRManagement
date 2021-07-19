43b518d7ed72ea4203c8ea4f1139ecc797c012dd
phuocleoceo <ht10082001@gmail.com>
2021-07-20 01:22:05 +0700

    SaveFile React

--- client-app/src/extension.js
+++ client-app/src/extension.js
@@ -1,11 +1,17 @@
 export function formatDateForBE(date) {
-    // yyyy-mm-dd => dd/mm/yyyy
-    var dateArray = date.split("-");
-    return dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
+	// yyyy-mm-dd => dd/mm/yyyy
+	var dateArray = date.split("-");
+	return dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
 }
 
 export function formatDateForFE(date) {
-    // dd/mm/yyyy => yyyy-mm-dd 
-    var dateArray = date.split("/");
-    return dateArray[2] + "-" + dateArray[1] + "-" + dateArray[0];
+	// dd/mm/yyyy => yyyy-mm-dd 
+	var dateArray = date.split("/");
+	return dateArray[2] + "-" + dateArray[1] + "-" + dateArray[0];
 }
+
+export const API_URL = "https://bsite.net/phuocleoceo/api";
+//export const API_URL = "https://localhost:5001/api";
+
+export const PHOTO_URL = "https://bsite.net/phuocleoceo/Photos/";
+//export const PHOTO_URL = "https://localhost:5001/Photos/";
