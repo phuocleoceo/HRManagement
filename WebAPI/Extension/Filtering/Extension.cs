3060a55ed8bec3f232088564bc67d9f30a2250a1
phuocleoceo <ht10082001@gmail.com>
2021-08-13 10:18:36 +0700

    Optimize Filtering

--- WebAPI/Extension/Filtering/Extension.cs
+++ WebAPI/Extension/Filtering/Extension.cs
@@ -6,28 +6,32 @@ namespace WebAPI.Extension.Filtering
 {
 	public static class Extension
 	{
+		// Linq to Entities cannot handle complex c.DateOfJoining.CalculateSeniority() 
+		// so we take it to minSeniority and  maxSeniority
 		public static DateTime CalculateMinSeniority(this int seniority)
 		{
-			int minYear = DateTime.Now.Year - seniority;
-			minYear = (minYear >= 1000) ? minYear : 1000;
-			return new DateTime(minYear, 1, 1);
+			int Year = DateTime.Now.Year - seniority;
+			Year = (Year >= 1000) ? Year : 1000;
+			return new DateTime(Year, 12, 31);
 		}
-
 		public static DateTime CalculateMaxSeniority(this int seniority)
 		{
-			int maxYear = DateTime.Now.Year + seniority;
-			maxYear = (maxYear <= 9999) ? maxYear : 9999;
-			return new DateTime(maxYear, 12, 31);
+			int Year = DateTime.Now.Year - seniority;
+			Year = (Year >= 1000) ? Year : 1000;
+			return new DateTime(Year, 1, 1);
 		}
 
+		// Exp : minSeniority = 1 ; maxSeniority = 20 ; YearNow = 2021
+		// 1/1/2001 <= X <= 31/12/2020
 		public static IQueryable<Employee> FilterSeniority(this IQueryable<Employee> list,
 											int minSeniority, int maxSeniority)
 		{
 			if (minSeniority >= 0 && maxSeniority != int.MaxValue)
 			{
+				DateTime minS = minSeniority.CalculateMinSeniority();
+				DateTime maxS = maxSeniority.CalculateMaxSeniority();
 				return list.Where(c =>
-					c.DateOfJoining >= minSeniority.CalculateMinSeniority() &&
-					c.DateOfJoining <= maxSeniority.CalculateMaxSeniority());
+					c.DateOfJoining >= maxS && c.DateOfJoining <= minS);
 			}
 			return list;
 		}
